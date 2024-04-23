d44e3b65b6a5c41c02e237f5263c19123505f506
VincentGuoW <sdevincentg@gmail.com>
2024-04-23 16:47:37 -0400

    dataType

---
+++ April2024/Class10/DataType.java
@@ -0,0 +1,51 @@
+package April2024.Class10;
+
+public class DataType {
+    public static void main(String[] args) {
+        byte a = 10;
+        System.out.println(a);
+
+        short b=20;
+        System.out.println(b);
+
+        int c = 30;
+        System.out.println(c);
+
+        long d = 9L;    //long add capital L at the end.
+        System.out.println(d);
+
+        float e = 1.1F; //float add capital F at the end.
+        System.out.println(e);
+
+        double f = 1.1;
+        System.out.println(f);
+        
+        char g = 'A';
+        System.out.println(g);
+
+        boolean h = true;
+        System.out.println(h);
+
+    }
+}
+
+/*Data type:
+
+    Integer
+        byte
+        short
+        int             *********************
+        long
+
+    Floating point
+        float
+        double          *********************
+
+    Character
+        char
+
+
+    Boolean
+        True
+        False
+ */
---
+++ April2024/Class9/variable.java
@@ -0,0 +1,17 @@
+package April2024.Class9;
+
+public class variable {
+    
+    public static void main(String[] args) {
+        int a=1, b=2, c=3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        /* set three variable at a time */
+
+        System.out.println(11);    //   number => Decimal  ==>        11
+        System.out.println(011);   //0 +number => Octal    ==>  8+1=  9
+        System.out.println(0b11);  //ob+number => Binary   ==>  2+1=  3
+    }
+
+}
