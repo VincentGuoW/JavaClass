a46c34b0ddef7dea4ec77f1e29e1d5bf9eb2ae02
VincentGuoW <sdevincentg@gmail.com>
2024-04-23 15:10:50 -0400

    /t tab learning

---
+++ April2024/Class8/tab_character.java
@@ -0,0 +1,18 @@
+package April2024.Class8;
+
+public class tab_character {
+    public static void main(String args[]){
+      
+        System.out.println("name12341234123"+ "\t"+"Age");
+        System.out.println("Vincentt"+"\t"+"12");
+
+        /* "\t" tab character
+            will auto fill the empty spot for 8 character a set.
+            if the previous one is 7 characters then it will fill up to 8(one empty).
+            if the previous one is 8 characters then it will fill up to 16(eight empty).
+            System.out.println("12345678\t1"); // This will output "12345678    1" with spaces instead of "\t"
+            System.out.println("123456\t12"); // This will output "123456  12" with spaces instead of "\t"
+          
+        */
+    }
+}
