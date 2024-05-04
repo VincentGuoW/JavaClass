7d092cc35d6b38e98fb2a456fcd4b3771fafa61a
VincentGuoW <sdevincentg@gmail.com>
2024-05-04 12:56:19 -0400

    test

---
+++ May2024/Class05/ClassDemo.java
@@ -0,0 +1,10 @@
+package May2024.Class05;
+
+public class ClassDemo {
+    public static void main(String[] args) {
+        Girlfriend gf = new Girlfriend();
+        gf.name="Yoki";
+        gf.height=172.3;
+        gf.sleep(gf.name,gf.height);
+    }
+}
---
+++ May2024/Class05/Girlfriend.java
@@ -0,0 +1,18 @@
+package May2024.Class05;
+
+public class Girlfriend {
+    String name;
+    Double height;
+    Double weight;
+
+    public static void sleep(String name,double height){
+        System.out.println( name + " is sleeping" + height);
+    }
+
+    public static void eat(){
+        System.out.println("is having a meal");
+        System.out.println("is having a meal");
+        System.out.println("is having a meal");
+        System.out.println("is having a meal");// mouse rolling key hold and toll!
+    }
+}
