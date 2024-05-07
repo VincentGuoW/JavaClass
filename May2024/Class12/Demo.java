b25ed6f2b532f7459d29b2f697a261ce3144d40e
VincentGuoW <sdevincentg@gmail.com>
2024-05-06 23:25:31 -0400

    test

---
+++ May2024/Class12/Demo.java
@@ -0,0 +1,5 @@
+package May2024.Class12;
+
+public class Demo {
+    
+}
--- May2024/Class12/ProductInfo.java
+++ May2024/Class12/ProductInfo.java
@@ -1,7 +1,29 @@
 package May2024.Class12;
 
 public class ProductInfo {
+    private int id;
     private String name;
     private int price;
     private int stock;
+
+    public ProductInfo(){
+    }
+    public ProductInfo(int id,String name,int price,int stock){
+        setId(id);
+        setName(name);
+        setPrice(price);
+        setStock(stock);
+    }
+    public void setId(int id){this.id=id;}
+    public int getId(){return this.id;}
+
+    public void setName(String name){this.name=name;}
+    public String getName(){return this.name;}
+
+    public void setPrice(int price){this.price=price;}
+    public int getPrice(){return this.price;}
+
+    public void setStock(int stock){this.stock=stock;}
+    public int getStock(){return this.stock;}
+
 }
