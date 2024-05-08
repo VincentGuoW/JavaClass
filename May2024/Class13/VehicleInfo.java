1ad9f5af2f8ee845b68487d058a51d283f20dcf0
VincentGuoW <sdevincentg@gmail.com>
2024-05-07 23:45:07 -0400

    update

---
+++ May2024/Class13/VehicleInfo.java
@@ -0,0 +1,42 @@
+package May2024.Class13;
+
+public class VehicleInfo {
+    private String brand;
+    private double price;
+    private String color;
+
+    public VehicleInfo(){
+        
+    }
+
+    public VehicleInfo(String brand, double price, String color) {
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
---
+++ May2024/Class13/VehicleInput.java
@@ -0,0 +1,51 @@
+package May2024.Class13;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class VehicleInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Please input your vehicle information below: ");
+        boolean moreVehicle = true;
+        int inputVehicleAmount = 0;
+        //VehicleInfo[][] vehicleArray = new VehicleInfo[1][];
+        ArrayList<VehicleInfo> vehicleArrayList = new ArrayList<>();
+        while(moreVehicle){
+            VehicleInfo vInfo = new VehicleInfo();
+            System.out.println("Vehicle brand: ");
+            vInfo.setBrand(sc.next());
+
+            System.out.println("Vehicle price: ");
+            vInfo.setPrice(sc.nextDouble());
+
+            System.out.println("Vehicle color: ");
+            vInfo.setColor(sc.next());
+            //vehicleArray[1][inputVehicleAmount]=vInfo;
+            vehicleArrayList.add(vInfo);
+            inputVehicleAmount++;
+
+            System.out.println("Is that all? ");
+            String input = sc.next();
+            if(input.equalsIgnoreCase("YES")|| input.equalsIgnoreCase("Y")){
+                moreVehicle=false;
+            }
+            
+        }
+        System.out.println("You have inputted "+ inputVehicleAmount
+                        +" vehicles; And here's vehicle detials: ");
+        for (VehicleInfo i : vehicleArrayList) {
+            int j=1;
+            System.out.println();
+            System.out.println("Vehicle Input "+j+" :");
+            System.out.print("Vehicle Brand : "+i.getBrand()+" || ");
+            System.out.print("Vehicle Price : $"+i.getPrice()+" || ");
+            System.out.print("Vehicle Color : "+i.getColor());
+            System.out.println();
+            System.out.println("=================");
+
+        }
+        System.out.println("Done");
+    }
+}
