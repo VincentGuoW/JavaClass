27ce68d1c7307a2b626363d598bb8b1df25ba7a5
VincentGuoW <sdevincentg@gmail.com>
2024-05-09 14:38:19 -0400

    String Study

---
+++ May2024/Class17/ApiDemo.java
@@ -0,0 +1,20 @@
+package May2024.Class17;
+
+import java.util.Scanner;
+
+public class ApiDemo {
+    /*
+     What is API?:
+        Scanner sc = new Scanner(System.in)  ==> This is an API.
+        Consider Api is cool function / class that other people prewrote it.
+        You dont need to know how that thing works, all you need to know is how to use it.
+        E.g. Iphone user, you dont need to build Iphone, you just need to know how to use Iphone.
+
+        !! JDK file !!
+     
+        */
+    public static void main(String[] args) {
+        Scanner
+        
+    }
+}
---
+++ May2024/Class18/StringStudy.java
@@ -0,0 +1,5 @@
+package May2024.Class18;
+
+public class StringStudy {
+    
+}
