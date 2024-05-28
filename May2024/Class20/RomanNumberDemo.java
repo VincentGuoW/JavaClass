834f55cc07669c5ea30bbd90352310b0c1b4ef90
VincentGuoW <sdevincentg@gmail.com>
2024-05-28 18:29:29 -0400

    How to use switch char to str

--- May2024/Class20/RomanNumberDemo.java
+++ May2024/Class20/RomanNumberDemo.java
@@ -17,19 +17,17 @@ public class RomanNumberDemo {
      * 0 " "
      */
     public static void main(String[] args) {
-        while(true){
-        System.out.println("Please put number in less than 9 digits");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        if (inputNumberOnly(input) && input.length() <= 9) {
-            System.out.println(romanNumberFunction(input));
-            break;
+        while (true) {
+            System.out.println("Please put number in less than 9 digits");
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+            if (inputNumberOnly(input) && input.length() <= 9) {
+                System.out.println(romanNumberFunction(input));
+                break;
+            } else {
+                System.out.println("Please retry your number.");
+            }
         }
-        else{
-            System.out.println("Please retry your number.");
-        }
-        }
-        
 
     }
 
@@ -46,28 +44,44 @@ public class RomanNumberDemo {
         return result;
     }
 
-    public static String romanNumberFunction(String input){
-        String [] arr = {" ","I","II","III","IV","V","VI","VII","VIII","IX"};
-        StringJoiner sj = new StringJoiner("-","[","]");
+    public static String romanNumberFunction(String input) {
+        String[] arr = { " ", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        StringJoiner sj = new StringJoiner("-", "[", "]");
         for (int i = 0; i < input.length(); i++) {
-            sj.add(arr[input.charAt(i)-48]);
+            sj.add(arr[input.charAt(i) - 48]);
         }
         return sj.toString();
     }
 
-
-    public static boolean checkStr(String str){
-        //better way, different return. All wrong options return false;
-        //if no catch wrong options, return true.
-        if(str.length()>9){
+    public static boolean checkStr(String str) {
+        // better way, different return. All wrong options return false;
+        // if no catch wrong options, return true.
+        if (str.length() > 9) {
             return false;
         }
         for (int i = 0; i < str.length(); i++) {
-            char c  = str.charAt(i);
-            if(c<'0'&&c>'9'){
+            char c = str.charAt(i);
+            if (c < '0' && c > '9') {
                 return false;
             }
         }
         return true;
+    }
+
+    public static String changeRomanWay2(char number){
+        String str = switch (number) {
+            case '0'->"";
+            case '1'->"I";
+            case '2'->"II";
+            case '3'->"III";
+            case '4'->"IV";
+            case '5'->"V";
+            case '6'->"VI";
+            case '7'->"VII";
+            case '8'->"VIII";
+            case '9'->"IX";
+            default ->"";
+        }
+        return str;
     }
 }
--- May2024/Class20/StringJoinerDemo.java
+++ May2024/Class20/StringJoinerDemo.java
@@ -8,7 +8,10 @@ public class StringJoinerDemo {
         int[] arr = {1,2,3,4,5};
         StringJoiner sj = new StringJoiner(",","[","]");
         for (int i = 0; i < arr.length; i++) {
-            sj.add(arr[i]+"");//why add with ""????????????????
+            sj.add(arr[i]+"");//why add with ""
+            //String + anything(e.g. int/char/byte...) => String
+            //The purpose of this add zero value String is
+            //Change INT TO STRING!!!!
           
         }
         System.out.println(sj.toString());
