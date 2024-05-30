f4c3a521b169918f6c1acb1601325c7994fdbb2b
VincentGuoW <sdevincentg@gmail.com>
2024-05-30 02:55:17 -0400

    Scanner beg

--- May2024/Class21/ArrayListDemo5.java
+++ May2024/Class21/ArrayListDemo5.java
@@ -10,12 +10,22 @@ public class ArrayListDemo5 {
         while (true) {
             System.out.println("Please input student Name: ");
             String studentName = sc.nextLine();
+
             System.out.println("Please input student Age: ");
             int studentAge = sc.nextInt();
+            sc.nextLine();
+            //e.g. "15/n" will be my enter, but they only take 15 for this section
+            //so need sc.nextLine() to receive the "/n", since we don't need it. Just live it here.
+
             Students newStudent = new Students(studentName,studentAge);
             studentList.add(newStudent);
+
             System.out.println("You done?(Y/N)");
-            String finish = sc.next();
+            String finish = sc.nextLine();
+            //String finish = sc.next(); next will only take string, but "/n". it will pass to the next section
+            //sc.nextLine();
+            System.out.println();
+
             if(finish.equals("Y")){ break;}
         }
         for (int i = 0; i < studentList.size(); i++) {
