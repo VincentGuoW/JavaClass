eed7cd23bd0e917f2837beb6988ab2d4aac0d102
VincentGuoW <sdevincentg@gmail.com>
2024-07-29 00:01:23 -0400

    test

--- July2024/Class01/a04staticdemo04/Student.java
+++ July2024/Class01/a04staticdemo04/Student.java
@@ -9,7 +9,7 @@ public class Student {
     //     System.out.println(name + " , " + age +  " , "  + teacherName);
     // }
 
-    //"Student this" ==> address ==>  ever use it.(s1, s2, s3....)
+    //"Student this" ==> address ==>  who ever use it.(s1, s2, s3....)
     public void infoShow(Student this){
         System.out.println("This address: "+this);
         System.out.println(name + " , " + age +  " , "  + teacherName);
@@ -19,6 +19,13 @@ public class Student {
     }
 
     public static void method(){
+        System.out.println(teacherName);//Use static value is fine.
+
+        //For static method, you can't use non static info,
+        //Cus you can't define which one you are using(can be s1, s2, s3....) e.g.
+        //System.out.println(age);
+        //System.out.println(this.age);
+
         System.out.println("Utility class!");
     }
 }
