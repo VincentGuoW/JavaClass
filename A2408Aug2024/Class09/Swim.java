2a7936e97df3e95696ffc014671f5dfe9e4a7fca
VincentGuoW <sdevincentg@gmail.com>
2024-08-11 11:56:08 -0400

    test

--- A2408Aug2024/Class09/Swim.java
+++ A2408Aug2024/Class09/Swim.java
@@ -4,5 +4,5 @@ public interface Swim {
     //interface is like a plug in, something command that most but not all class need
     public abstract void swim();
     //mostly will be public abstract finish 
-    //abstract makesure people use this interface must rewrite it 
+    //abstract makesure when people use this interface must rewrite it 
 }
