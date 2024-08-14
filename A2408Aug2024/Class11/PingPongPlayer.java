02b046a2272dc6c71538b3138cd7c68b335eac1d
VincentGuoW <sdevincentg@gmail.com>
2024-08-14 14:36:30 -0400

    test

--- A2408Aug2024/Class11/PingPongPlayer.java
+++ A2408Aug2024/Class11/PingPongPlayer.java
@@ -11,7 +11,7 @@ public class PingPongPlayer extends PlayerDemo implements EnglishSpeaking{
 
     @Override
     public void job() {
-        System.out.println("Ping Pong player learn how to Ping Pong");
+        System.out.println("Ping Pong player learn Ping Pong");
     }
 
     @Override
