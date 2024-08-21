17ff772cd34193dc7e387cf62d48950706056f21
VincentGuoW <sdevincentg@gmail.com>
2024-08-21 06:42:04 -0400

    test

---
+++ PjPuzzleGame/src/com/vincent/test/MyActionListener.java
@@ -0,0 +1,12 @@
+package PjPuzzleGame.src.com.vincent.test;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class MyActionListener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("The button been clicked");
+    }
+    
+}
---
+++ PjPuzzleGame/src/com/vincent/test/Test2.java
@@ -0,0 +1,44 @@
+package PjPuzzleGame.src.com.vincent.test;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Test2 {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+
+        jFrame.setSize(603, 680);
+
+        jFrame.setTitle("Sample");
+
+        jFrame.setAlwaysOnTop(true);
+
+        jFrame.setLocationRelativeTo(null);
+
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        jFrame.setLayout(null);
+
+        JButton jButton = new JButton("Click me");
+        jButton.setBounds(0, 0, 100, 50);
+
+        //jButton.addActionListener(new MyActionListener());
+
+        jButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              System.out.println("Click once");  }
+                
+            }
+        );
+
+        jFrame.getContentPane().add(jButton);
+
+        jFrame.setVisible(true);
+    }
+}
