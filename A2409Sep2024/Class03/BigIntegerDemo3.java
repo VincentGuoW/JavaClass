e9a70eba969b975eedf4816b5d6909e2fd2945c9
VincentGuoW <sdevincentg@gmail.com>
2024-09-11 20:37:29 -0400

    test

---
+++ A2409Sep2024/Class03/BigIntegerDemo3.java
@@ -0,0 +1,12 @@
+package A2409Sep2024.Class03;
+
+import java.math.BigInteger;
+
+public class BigIntegerDemo3 {
+    public static void main(String[] args) {
+        BigInteger bi1 = new BigInteger("27670116110564327424");
+        System.out.println(bi1);
+        //wrong int value will return -2147483648
+    
+    }
+}
