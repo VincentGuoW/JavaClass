56cb660cbd0cf8a14aecf780e620890ec934b363
VincentGuoW <sdevincentg@gmail.com>
2024-12-07 07:14:02 -0500

    test

--- A2411Nov2024/Class04/LambdaDemo.java
+++ A2411Nov2024/Class04/LambdaDemo.java
@@ -26,7 +26,7 @@ public class LambdaDemo {
         });
 
         //list.forEach(new Consumer<T ==> put in list's type>(){})==>
-        //==>Add unimplementmethod==Get result ==>
+        //==>Add unimplementmethod==Get result 
         list.forEach(new Consumer<String>() {
 
             @Override
