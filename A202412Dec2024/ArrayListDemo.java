364d987590d24dcd83f109d6caf430d27e412687
VincentGuoW <sdevincentg@gmail.com>
2024-12-09 17:33:48 -0500

    TEST

---
+++ A202412Dec2024/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package A202412Dec2024;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        System.out.println();
+        ArrayList<String> ar1 = new ArrayList<>();
+        System.out.println(ar1.size());// = 0
+
+        ar1.add("1");
+        System.out.println(ar1.size());// = 1
+        //  ==>"1" means 2 things
+        //      1.current amount
+        //      2.next element input index
+    }
+}
