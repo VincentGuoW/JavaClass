3c5217b8c6ded78f1dd814b36bbfd8d235ecc5f2
VincentGuoW <sdevincentg@gmail.com>
2025-01-07 20:50:37 -0500

    Iterator

---
+++ A202501Jan2025/Class02/IteratorDemo.java
@@ -0,0 +1,20 @@
+package A202501Jan2025.Class02;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class IteratorDemo {
+    public static void main(String[] args) {
+        //Collection<String> c = new ArrayList<>();
+        ArrayList<String> c = new ArrayList<>();
+        c.add("aaa");
+        c.add("bbb");
+        c.add("ccc");
+        c.add("ddd");
+        Iterator<String> it = c.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
