f3a256f2972f8696655e41fef1967de16c5778f0
VincentGuoW <sdevincentg@gmail.com>
2025-01-10 21:35:10 -0500

    test

--- A202501Jan2025/Class02/IteratorDemo.java
+++ A202501Jan2025/Class02/IteratorDemo.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 public class IteratorDemo {
     public static void main(String[] args) {
         //Collection<String> c = new ArrayList<>();
-        //Collection is above arraylist//
+        //Collection is above arraylist
         ArrayList<String> c = new ArrayList<>();
         c.add("aaa");
         c.add("bbb");
