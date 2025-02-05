497073811b5c26271defc1da8de3dd0fc21cd035
VincentGuoW <sdevincentg@gmail.com>
2025-02-04 19:02:36 -0500

    test

--- A202502Feb2024/Class01/GenericsDemo3.java
+++ A202502Feb2024/Class01/GenericsDemo3.java
@@ -3,16 +3,19 @@ package A202502Feb2024.Class01;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class GenericsDemo2 {
+public class GenericsDemo3{
     public static void main(String[] args) {
-        //Best way to set up type from beginning.
-        //E.g.
-        //ArrayList<String> arrayList = new ArrayList<String>();
+        
         System.out.println();
-        ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add("aaa");
-        //arrayList.add(111);
-        //arrayList.add(new Student("name",12,"CS")); 
+        //Must be Integer, can't be int
+        
+        //Int       saved in ==> stack
+        //Integer   saved in ==> heap
+        ArrayList<Object> list = new ArrayList<>();
+        //ArrayList Generics must be an Object, not fast saving Int in stack.
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(123);
+       
 
         Iterator it = arrayList.iterator();
         while (it.hasNext()) {
