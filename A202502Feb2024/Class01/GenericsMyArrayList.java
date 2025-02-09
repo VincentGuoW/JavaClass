810c507ddde61095e8dbdccf2bbbe48126934bf6
VincentGuoW <sdevincentg@gmail.com>
2025-02-09 04:24:04 -0500

    test

--- A202502Feb2024/Class01/GenericsMyArrayList.java
+++ A202502Feb2024/Class01/GenericsMyArrayList.java
@@ -1,8 +1,23 @@
 package A202502Feb2024.Class01;
 
+import java.util.Arrays;
+
 public class GenericsMyArrayList<E> {
     Object[] obj = new Object[10];
     int size;
     //test
+
+    public boolean add(E e){ //E type's e (E e)//(E sampleName)
+        obj[size] = e;
+        size++;
+        return true;
+    }
+
+    public E get(int index){return (E)obj[index];}
+
+    @Override
+    public String toString() {
+        return Arrays.toString(obj);
+    }
     
 }
