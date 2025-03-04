0f3a2afa1cb993bcbcbeab64b781373fc9e1b7d0
VincentGuoW <sdevincentg@gmail.com>
2025-03-04 17:08:33 -0500

    Hashcode

---
+++ A202503Mar2025/Class01/SetDemo4.java
@@ -0,0 +1,8 @@
+package A202503Mar2025.Class01;
+
+public class SetDemo4 {
+    public static void main(String[] args) {
+        //1. If not rewrite HashCode --> Diff Address == Diff HashCode
+        //2. If rewrite HashCode --> Same Value == Same HashCode
+    }
+}
