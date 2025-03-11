826964fbd42f0b17f69ce2a24246519305f6ef0d
VincentGuoW <sdevincentg@gmail.com>
2025-03-11 16:07:39 -0400

    Hash Map study

---
+++ A202503Mar2025/Class08/HashMapDeepLearning1.java
@@ -0,0 +1,16 @@
+package A202503Mar2025.Class08;
+
+import java.util.HashMap;
+
+public class HashMapDeepLearning1 {
+    /*
+        View the source code of HashMap
+            Open the source code ==> Use above search and type "@"
+            Open the source code ==> Ctrl + Ult + "O"
+    */
+    public static void main(String[] args) {
+        HashMap<String,String> hm = new HashMap<>();
+        hm.clear();
+
+    }
+}
