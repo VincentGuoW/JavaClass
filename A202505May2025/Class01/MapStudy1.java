0f1ac64ad227c7a0ae105b3fc63447e9829999b6
VincentGuoW <sdevincentg@gmail.com>
2025-05-05 19:50:49 -0400

    Hashmap study

---
+++ A202505May2025/Class01/MapStudy1.java
@@ -0,0 +1,12 @@
+package A202505May2025.Class01;
+
+public class MapStudy1 {
+    public static void main(String[] args) {
+        /*
+        HashMap() method==> 
+            compute() 
+                1.extends  (keep the same)
+                2.override (write sth diff)
+         */
+    }
+}
