1db7d7b1becba7c512288afd86d4d409ea857448
VincentGuoW <sdevincentg@gmail.com>
2025-05-06 15:53:05 -0400

    error

---
+++ A202505May2025/Class01/HashMapStudy3.java
@@ -0,0 +1,5 @@
+package A202505May2025.Class01;
+
+public class HashMapStudy3 {
+    
+}
--- A202505May2025/Class01/test.java
+++ A202505May2025/Class01/test.java
@@ -1,6 +1,6 @@
 package A202505May2025.Class01;
 
-public class HashMapStudy1 {
+public class test {
     public static void main(String[] args) {
         System.out.println("Test");
     }
